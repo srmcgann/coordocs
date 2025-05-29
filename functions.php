7af8f5d55916360b1f9d5b3ef3aecf6224ee50fd
Scott R McGann <s.r.mcgann@hotmail.com>
2025-05-29 14:14:01 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
   require_once('db.php');
-  echo 123 . "\n";
+  $sql = "SELECT * FROM projects";
+  $res = mysqli_query($link, $sql);
+  for($i = 0; $i<mysqli_num_rows($res); ++$i){
+    $row = mysqli_fetch_assoc($res);
+    echo 'project id -> ' . $row['id'] . "\n";
+  }
 ?>
